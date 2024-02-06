f5bc5fcc5194001cc56ca9ac1029cdb1fcc1ba34
danmor23daw2 <15586175@fje.edu>
2024-02-06 03:59:35 +0100

    Consumidor Acabado

--- WebServiceCliente.java
+++ WebServiceCliente.java
@@ -3,11 +3,10 @@ package proyecto.cartas.proyectocartas;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+
 
 
 public class WebServiceCliente {
